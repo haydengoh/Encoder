40e50864a0ccd00237578501f79aa2f06db86236
haydengoh <103562098+haydengoh@users.noreply.github.com>
2023-03-14 11:45:19 +0800

    Update Tester.java

--- DXC/src/com/dxc/Tester.java
+++ DXC/src/com/dxc/Tester.java
@@ -6,6 +6,7 @@ class Conversion {
 	static char offsetChar = 'B';
 
 	public static int findIndex(char offsetChar) {
+		// finding the index of the offset char
 		int offsetIndex = 0;
 		for (int i = 0; i < ref.length(); i++) {
 			if (offsetChar == ref.charAt(i)) {
@@ -24,12 +25,10 @@ class Conversion {
 		for (int i = 0; i < ref.length() - offsetIndex; i++) {
 			sb.append(ref.charAt(i));
 		}
-
 		return sb.toString();
 	}
 
 	public static String encode(String plainText) {
-
 		// find offset char index
 		int offsetIndex = findIndex(offsetChar);
 
@@ -41,9 +40,11 @@ class Conversion {
 		encodedSB.append(ref.charAt(offsetIndex)); // append offset char
 		for (int i = 0; i < plainText.length(); i++) {
 			for (int j = 0; j < newRef.length(); j++) {
+				// if plainText is space
 				if (plainText.charAt(i) == ' ') {
 					encodedSB.append(" ");
 					break;
+					// if plainText char == ref char
 				} else if (plainText.charAt(i) == ref.charAt(j)) {
 					encodedSB.append(newRef.charAt(j)); // append encoded char
 				}
@@ -53,6 +54,7 @@ class Conversion {
 	}
 
 	public static String decode(String encodedText) {
+		// set offset char as first index of encodedText
 		char offsetChar = encodedText.charAt(0);
 
 		// find offset char index
@@ -65,9 +67,11 @@ class Conversion {
 		StringBuilder decodedSB = new StringBuilder();
 		for (int i = 1; i < encodedText.length(); i++) {
 			for (int j = 0; j < newRef.length(); j++) {
+				// if encodedText is space
 				if (encodedText.charAt(i) == ' ') {
 					decodedSB.append(" ");
 					break;
+					// if encodedText char == new ref char
 				} else if (encodedText.charAt(i) == newRef.charAt(j)) {
 					decodedSB.append(ref.charAt(j)); // append decoded char
 				}
@@ -81,5 +85,9 @@ public class Tester {
 	public static void main(String[] args) {
 		System.out.println("Encode: " + Conversion.encode("HELLO WORLD"));
 		System.out.println("Decode: " + Conversion.decode("FC/GGJ RJMG."));
+
+		System.out.println("Encode: " + Conversion.encode("BEAUTIFUL DAY"));
+		System.out.println("Decode: " + Conversion.decode("BAD/TSHETK C/X"));
+
 	}
 }
