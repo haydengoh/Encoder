e80438e88d85147e573f3eaf310e070315985e53
haydengoh <103562098+haydengoh@users.noreply.github.com>
2023-03-15 13:29:06 +0800

    Update Tester.java

--- DXC/src/com/dxc/Tester.java
+++ DXC/src/com/dxc/Tester.java
@@ -2,13 +2,13 @@ package com.dxc;
 
 public class Tester {
 	public static void main(String[] args) {
-		Conversion c = new Conversion("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789()*+,-./", 'G');
+		Conversion c = new Conversion('G');
 		String test1 = c.encode("HELLO WORLD");
 		String test2 = c.decode("BGDKKN VNQKC");
 		System.out.println(test1);
 		System.out.println(test2);
 		
-		Conversion c2 = new Conversion("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789()*+,-./", 'K');
+		Conversion c2 = new Conversion('K');
 		String test3 = c2.encode("BEAUTIFUL DAY");
 		String test4 = c2.decode("BAD/TSHETK C/X");
 		System.out.println(test3);
